25fb0d7df47a162743f68bb74211952d84653b2e
Andrew Tedder <54320061+DrATedder@users.noreply.github.com>
2023-11-30 09:35:13 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OTU_predictor',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     package_data={'OTU_predictor': ['model/*.joblib']},
     install_requires=[
